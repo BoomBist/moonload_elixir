982b17403ca27ba0c4941b4a7fa3db890b41d49c
grumpydevelop <simsim0331@gmail.com>
2017-02-04 13:34:20 +0300

    Add files via upload

---
+++ qt4.py
@@ -0,0 +1,9 @@
+class settings:
+    url = 'https://download.qt.io/archive/qt/4.8/4.8.0/qt-everywhere-opensource-src-4.8.0.tar.gz'
+    package = 'qt-everywhere-opensource-src-4.8.0.tar.gz'
+    name = 'QT4 Libraries'
+    foldername = 'qt-everywhere-opensource-src-4.8.0'
+    desc = 'Qt is used for developing multi-platform applications and graphical user interfaces (GUIs)'
+class install:
+    configure = 'cd qt-everywhere-opensource-src-4.8.0 && ./configure'
+    build = 'cd qt-everywhere-opensource-src-4.8.0 && sudo make install'
