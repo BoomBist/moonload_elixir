7f77422661dd3d7ca538b506d173736a90d4e2d0
grumpydevelop <simsim0331@gmail.com>
2017-02-06 18:14:21 +0300

    Update boost.py

--- boost.py
+++ boost.py
@@ -1,5 +1,6 @@
 class settings:
     url = 'https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.tar.gz'
+    url2 = 'https://github.com/boostorg/boost/archive/boost-1.63.0.tar.gz'
     package = 'boost_1_63_0.tar.gz'
     name = 'Boost'
     desc = 'Boost is a set of libraries for the C++ programming language that provide support for tasks and structures such as linear algebra, pseudorandom number generation, multithreading, image processing, regular expressions, and unit testing'
