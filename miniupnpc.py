f6b04955ad83e1beb4c0ecf4353705f4e31d71cd
grumpydevelop <simsim0331@gmail.com>
2017-02-06 17:35:40 +0300

    Update miniupnpc.py

--- miniupnpc.py
+++ miniupnpc.py
@@ -1,3 +1,4 @@
+import sys
 class settings:
     url = 'https://miniupnp.tuxfamily.org/files/download.php?file=miniupnpd-2.0.20161216.tar.gz'
     package = 'miniupnpd-2.0.20161216.tar.gz'
@@ -6,4 +7,7 @@ class settings:
     desc = 'The UPnP protocol is supported by most home adsl/cable routers and Microsoft Windows 2K/XP. The aim of the MiniUPnP project is to bring a free software solution to support the "Internet Gateway Device" part of the protocol.'
 class install:
     configure = 'cd miniupnpd-2.0.20161216 && ./genconfig.sh'
-    build = 'cd miniupnpd-2.0.20161216 && make -f Makefile.linux'
+    if sys.platform == 'linux':
+        build = 'cd miniupnpd-2.0.20161216 && make -f Makefile.linux'
+    elif sys.platform == 'darwin':
+        build = 'cd miniupnpd-2.0.20161216 && make -f Makefile.macosx'
